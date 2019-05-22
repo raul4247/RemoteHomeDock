18bd7ad2cf4f861172c906262049b8ba89c1c8a5
Raul <raulfmansur@hotmail.com>
2019-05-22 00:31:05 -0300

    axios get request implemented

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 import { TouchableOpacity, Text } from 'react-native'
+import axios from 'axios'
 
 export default class HomeScreen extends Component {
     makeRequest() {
-        console.log('hey')
+        let url = 'http://192.168.0.4:3000/'
+        axios.get(url)
+            .then(res => {
+                console.log(res.data)
+            })
     }
     render() {
         return (
