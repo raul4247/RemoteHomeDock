4a88a7305c1fdbb7a0bfef70b91d8e57f3edd57a
Raul <raulfmansur@hotmail.com>
2019-05-22 07:53:42 -0300

    Input text on config screen

--- src/screens/ConfigScreen.js
+++ src/screens/ConfigScreen.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react'
-import { TouchableOpacity, View, Text } from 'react-native'
+import { View, TextInput } from 'react-native'
 import axios from 'axios'
 
 export default class ConfigScreen extends Component {
-    makeRequest() {
-        let url = 'http://192.168.0.4:3000/'
-        axios.get(url)
-            .then(res => {
-                console.log(res.data)
-            })
+    constructor(props) {
+        super(props)
+        this.state = { ipAddress: '0', port: '0'}
     }
     render() {
         return (
             <View>
-            <TouchableOpacity onPress={this.makeRequest}>
-                <Text>Configs</Text>
-            </TouchableOpacity>
+                <TextInput
+                    onChangeText={(val) => this.setState({ ipAddress: val })}
+                    keyboardType={"numeric"}
+                    value={this.state.ipAddress}
+                />
+                <TextInput
+                    onChangeText={(val) => this.setState({ port: val })}
+                    keyboardType={"numeric"}
+                    value={this.state.port}
+                />
             </View>
         )
     }
