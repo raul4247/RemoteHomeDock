ff549af7709dc1819d87a2615b8fa225f2b03278
Raul <raulfmansur@hotmail.com>
2019-05-24 17:00:20 -0300

    New fonts and icons

--- android/app/src/main/java/com/remotehomedock/MainApplication.java
+++ android/app/src/main/java/com/remotehomedock/MainApplication.java
@@ -3,6 +3,9 @@ package com.remotehomedock;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.horcrux.svg.SvgPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new SvgPackage(),
+            new AsyncStoragePackage(),
             new RNGestureHandlerPackage()
       );
     }
